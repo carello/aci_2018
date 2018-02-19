48b14c39489cd753a997b46b9b5d08911bb55352
carello <cp2carello@gmail.com>
2018-02-18 20:43:40 -0600

    working with json.load (data dir)

---
+++ data/apic_dhcp1.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from pprint import pprint
+
+with open('off_dhcpLease.json') as f:
+    data = json.load(f)
+
+print('\n==================================')
+pprint(data)
+
+print('\n==================================')
+stuff = data['imdata']
+for d in stuff:
+    print('{0:20}: {1:20}'.format(d['dhcpLease']['attributes']['clientId'], d['dhcpLease']['attributes']['ip']))
+
+print('\n==================================')
+with open('off_dhcpPool.json') as g:
+    g_data = json.load(g)
+
+pprint(g_data)
+
+print('\n==================================')
+with open('off_opflexODev.json') as h:
+    h_data = json.load(h)
+
+pprint(h_data)
+
+print('\n==================================')
+with open('off_topSystem.json') as j:
+    j_data = json.load(j)
+
+pprint(j_data)
